2cab981c6f9ce7b127743b32c007806a22206dbe
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-05-05 02:52:38 +0300

    Update 1.1

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AspNetCoreIdentityApp.Areas.Admin.Models;
+using AspNetCoreIdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreIdentityApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]    
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Route("/Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/Admin/UserList")]
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+            var userViewModelList = userList.Select(a => new UserViewModel()
+            {
+                UserID = a.Id,
+                UserName = a.UserName,
+                UserEmail = a.Email
+            }).ToList();
+
+
+
+
+            return View(userViewModelList);
+        }
+    }
+}
---
+++ Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreIdentityApp.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
